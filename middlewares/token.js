5405ad7e5e72635d65c11f5c59fe113d98288d0e
njmercado <njmercado@uninorte.edu.co>
2021-03-12 16:18:33 -0500

    Creados middlewares. Creado middleware de jwt para las sesiones

---
+++ middlewares/token.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken')
+const secretKey = process.env.SECRET_KEY
+
+module.exports = class Token {
+    constructor(){}
+
+    create(data){
+        const oneDay = 24*60*60
+        return jwt.sign(data, secretKey, {expiresIn: `${oneDay}`})
+    }
+
+    async verify(req, res, next) {
+        const token = req.body.token
+        if(token === null) res.status(400).send({
+            message: "Given token is empty",
+            error: true,
+            data: null,
+        })
+
+        jwt.verify(token, secretKey, (error, user) => {
+            console.error("Error verifying token: ", error)
+            if(error) {
+                res.status(403).send({ message: "You do not have access to this area", error: true, data: null })
+                return;
+            }
+            req.user = user
+            next()
+        })
+    }
+}
