e4f257aabd5d31424e80dae77fcc5c0facc87cbb
njmercado <njmercado@uninorte.edu.co>
2021-03-12 17:48:15 -0500

    Solucionado error de los pagos

--- routes/login.js
+++ routes/login.js
@@ -1,21 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const Charge = require('../utils/queries/charge')
+const charge = new Charge()
 const User = require('../utils/queries/user')
 const user = new User()
 const Token = require('../middlewares/token');
 const token = new Token()
 
-router.get('/', user.verify, (req, res, next) => {
+router.get('/', user.verify, async (req, res, next) => {
     try{
-        const {email, password} = req.body
-        const newToken = token.create({email, password})
-        res.status(200).send({
-            message: "token created",
-            error: false,
-            data: {
-                token: newToken
-            }
+        const newToken = token.create(req.body)
+        charge.getAllCharges(req.body.email).then((data) => {
+            console.log("data after much",data)
+            res.status(200).send({
+                message: "token created",
+                error: false,
+                data: {
+                    token: newToken,
+                    charges: data 
+                }
+            })
         })
+        
     }catch(error) {
         res.status(500).send({
             message: `Some error has happend: ${error}`,
