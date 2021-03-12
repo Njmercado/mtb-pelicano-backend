9328ebd8f2ac86e32d71e88973d4c95225a8c25a
njmercado <njmercado@uninorte.edu.co>
2021-03-12 17:47:00 -0500

    Agregada función para poder verificar si es admin

--- middlewares/token.js
+++ middlewares/token.js
@@ -6,7 +6,34 @@ module.exports = class Token {
 
     create(data){
         const oneDay = 24*60*60
-        return jwt.sign(data, secretKey, {expiresIn: `${oneDay}`})
+        return jwt.sign(data, secretKey, {expiresIn: `${oneDay}s`})
+    }
+
+    async verifyAdmin(req, res, next) {
+        const token = req.body.token
+        if(token === null) res.status(400).send({
+            message: "Given token is empty",
+            error: true,
+            data: null,
+        })
+
+        jwt.verify(token, secretKey, (error, user) => {
+            console.error("Error verifying token: ", error)
+            if(error) {
+                res.status(403).send({ message: error, error: true, data: null })
+                return;
+            }
+            if(user.role !== 'admin') {
+                res.status(400).send({
+                    message: "You do not have access to this area",
+                    error: true,
+                    data: null
+                })
+                return
+            }
+            req.user = user
+            next()
+        })
     }
 
     async verify(req, res, next) {
@@ -20,7 +47,7 @@ module.exports = class Token {
         jwt.verify(token, secretKey, (error, user) => {
             console.error("Error verifying token: ", error)
             if(error) {
-                res.status(403).send({ message: "You do not have access to this area", error: true, data: null })
+                res.status(403).send({ message: error, error: true, data: null })
                 return;
             }
             req.user = user
