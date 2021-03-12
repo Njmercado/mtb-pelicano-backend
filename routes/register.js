f08a8362d430de7300ce743320f81b3e2e109792
njmercado <njmercado@uninorte.edu.co>
2021-03-12 17:49:32 -0500

    Minor changes

--- routes/register.js
+++ routes/register.js
@@ -6,22 +6,20 @@ const user = new User()
 
 router
     .post('/', 
-    [
-
-    ],
     async (req, res, next) => {
-    try{
+        try{
 
-        const {document_number, email, password, name, last_name, role, phone} = req.body
-        const response = await user.verifyIfUserExist(email)
-        if(response[0].email) {
-            await user.createUser({email, password, name, last_name, role, document_number, phone})
-            await res.status(200).json({ message: 'User created successfully', error: false, data: null })
+            const {document_number, email, password, name, last_name, role, phone} = req.body
+            const response = await user.verifyIfUserExist(email)
+            if(!response[0]) {
+                user.createUser({email, password, name, last_name, role, document_number, phone})
+                res.status(200).json({ message: 'User created successfully', error: false, data: null })
+            }
+            res.status(200).json({ message: 'This user already exist', error: true, data: null })
+        } catch(error) {
+            res.status(400).json({ message: `Some error has happend: ${error}`, error: false, data: null })
         }
-        res.status(200).json({ message: 'This user already exist', error: true, data: null })
-    } catch(error) {
-        res.status(400).json({ message: `Some error has happend: ${error}`, error: false, data: null })
     }
-});
+);
 
 module.exports = router;
--- utils/queries/user.js
+++ utils/queries/user.js
@@ -9,12 +9,23 @@ module.exports = class User {
 
     async verify(req, res, next) {
         const {email, password} = req.body
-        console.log(req.body)
         let query = `select * from user where email = ? and password = ?;`
         const inserts = [email, password]
         query = mysqlDB.format(query, inserts)
         const response = await mysqlDB.query(query)
-        if(response[0]) { next() }
+        if(response[0]) { 
+            req.body = {
+                email: response[0].email,
+                document_number: response[0].document_number,
+                password: response[0].password,
+                name: response[0].name,
+                last_name: response[0].last_name,
+                phone: response[0].phone,
+                role: response[0].role,
+                charge_fulfilled: response[0].charge_fulfilled,
+            } 
+            next()
+        }
         else res.status(400).send({
             message: "Invalid credentials",
             error: true,
@@ -23,9 +34,9 @@ module.exports = class User {
         // return response
     }
 
-    async verifyIfUserExist(user_email){
+    async verifyIfUserExist(email){
         let query = `select email from user where email = ?;`
-        const inserts = [user_email]
+        const inserts = [email]
         query = mysqlDB.format(query, inserts)
         const response = await mysqlDB.query(query)
         return response
